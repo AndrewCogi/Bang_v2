1efcc723e76d4ad99405a48a53ecd93b1c233633
CHOHYUNSIK <whgustlr0326@gmail.com>
2021-11-04 14:48:09 +0000

    21-11-04 11:50 check connection of client/server and server stop is safe

--- BangServer/src/main/java/server/App.java
+++ BangServer/src/main/java/server/App.java
@@ -1,3 +1,7 @@
+/*
+ * class name: App
+ * purpose: get command & manage socket
+ */
 package server;
 
 import java.io.IOException;
@@ -10,44 +14,79 @@ import java.util.concurrent.Executors;
 public class App {
 	public static void main(String[] args) throws IOException{
 		// server start
-		System.out.println("Server Start");
+		System.out.println("[System] > Server Start");
 
 		// open server socket
-		ServerSocket listener = new ServerSocket(2222);;
+		ServerSocket listener = new ServerSocket(2222);
+		System.out.println("[System] > ServerSocket is opened (port: " + listener.getLocalPort() + ")");
 
-		// allocate 20 Threads
-		ExecutorService pool = Executors.newFixedThreadPool(20);
+		// allocate 10 Threads
+		int threadPoolNum = 10;
+		ExecutorService pool = Executors.newFixedThreadPool(threadPoolNum);
+		System.out.println("[System] > Make ThreadPools (10)");
 
-		// commands
-		new Thread(new Runnable(){
+		// client waiting
+		Thread waiter = new Thread(new Runnable(){
 			public void run(){
-				Scanner sc = new Scanner(System.in);
-				String cmd;
-				while(true){
-					cmd = sc.nextLine();
-					switch(cmd){
-						// stop server
-						case "/stop":
-							System.out.println("Server closing...");
-							sc.close();
-							try{
-								listener.close();
-							} catch(IOException e){
-								System.out.println("[ERROR] while closing...");
-								System.out.println(e.getMessage());
-							}
+				while(!Thread.interrupted()){
+					try{
+						Socket sock = listener.accept();
+						Capitalizer cap = new Capitalizer(sock);
+						pool.execute(cap);
+						System.out.println("[Connected] > " + sock);
+						System.out.print(">> ");
+					} catch(IOException e){
+						// if thead is stopped
+						if(Thread.interrupted() == true) {
+							System.out.println("[System] > Thread(socket) stopped.");
 							break;
+						}
+						// else accept error occured
+						else{
+							System.out.println("[ERROR] > while accepting listener.");
+							System.out.println(e.getMessage());
+						}
 					}
 				}
 			}
-		}).start();
-		// client waiting
-		while(true){
-			Socket sock = listener.accept();
-			Capitalizer cap = new Capitalizer(sock);
-			pool.execute(cap);
-			System.out.println("[Connected] > "+ sock);
-		}
+		});
+		// thread start
+		waiter.start();
 
+		// command
+		while(true){
+			Scanner sc = new Scanner(System.in);
+			String cmd;
+			while(true){
+				System.out.print(">> ");
+				cmd = sc.nextLine();
+				switch(cmd){
+					// stop server
+					case "stop":
+						System.out.println("[System] > Server closing...");
+						try{
+							// Scanner & listener & Thread stop
+							sc.close();
+							System.out.println("[System] > Scanner(sc) stopped.");
+							listener.close();
+							System.out.println("[System] > ServerSocket(listener) stopped.");
+							waiter.interrupt();
+							System.out.println("[System] > Thread(waiter) interrupted.");
+							// System exit
+							System.exit(0);
+						} catch(IOException e){
+							System.out.println("[ERROR] > while closing server.");
+							System.out.println(e.getMessage());
+						}
+						break;
+					// nothing..
+					case "":
+						break;
+					// unknown command
+					default:
+						System.out.println("[System] > Unknown command.");
+				}
+			}
+		}
 	}
 }
